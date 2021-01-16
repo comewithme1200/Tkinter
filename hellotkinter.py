a94f8f90fc76c9b1286931618525d60732ec52b1
TN <comewithme1200@gmail.com>
2021-01-16 22:53:54 +0700

    Tkinter

--- hellotkinter.py
+++ hellotkinter.py
@@ -6,7 +6,7 @@ def myclick():
 mylabel1 = Label(win, text='My name is Long')
 myButton = Button(win, text='Encrypt' , command=myclick)
 myButton.grid(row=2,column=1)
-mylabel1.grid(row=1, column=1)
+mylabel1.grid(row=5, column=1)
 win.mainloop()
 
 
